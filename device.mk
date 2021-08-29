722530845ad89039a7dcea38d542bfb9a02b6256
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-08-29 16:34:22 +0200

    RMX1921: import AudioFX from lineage and hide it from launcher

--- device.mk
+++ device.mk
@@ -180,9 +180,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# GalaxyParts
+# RealmeParts
 PRODUCT_PACKAGES += \
     RealmeDoze \
+    RealmeFX \
     RealmeProximityHelper \
     RealmeParts
 
